1f2384ad8c34fa9e3c36e271550aa4e39d4ca26b
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2022-06-16 13:47:56 -0300

    getById deleteById updateById deleBydID feitos mas ainda não testados

--- src/shared/services/api/client/ClientService.ts
+++ src/shared/services/api/client/ClientService.ts
@@ -25,6 +25,7 @@ interface IClient {
 }
 
 interface IDetalheClient {
+    id: string,
     name: string,
     cpf: string,
     email: string,
@@ -52,17 +53,45 @@ const getAll = async(): Promise<TClientComTotalCount | Error> => {
         return new Error((error as  { message: string }).message || 'Erro ao listar novos registros')
     }
 }
-const getById = async(): Promise<any> => {
-
+const getById = async(id: number): Promise<IDetalheClient | Error> => {
+    try {
+        const { data } = await api.get(`/api/client/${id}`)
+        if (data){
+            return data
+        }
+        return new Error('Erro ao consultar o registro T-T')
+    } catch (error) {
+        console.error(error)
+        return new Error((error as  { message: string }).message || 'Erro ao consultar o registro T-T')
+    }
 }
-const create = async(): Promise<any> => {
-
+const create = async(dados: Omit<IDetalheClient, 'id'>): Promise<string | Error> => {
+    try {
+        const { data } = await api.post<IDetalheClient>('/api/client', dados)
+        if (data){ 
+            return data.id
+        }
+        return new Error('Erro ao criar o registro u-u')
+    } catch (error) {
+        console.error(error)
+        return new Error((error as  { message: string }).message || 'Erro ao criar o registro u-u')
+    }
 }
-const updateById = async(): Promise<any> => {
-
+const updateById = async(id: string, dados: IDetalheClient): Promise<void | Error> => {
+    try {
+      await api.put<IDetalheClient>(`/api/client${id}`, dados)
+    } catch (error) {
+        console.error(error)
+        return new Error((error as {message: string}).message || 'Erro ao atualizar os dados do registro')
+    }
 }
-const deleById = async(): Promise<any> => {
-
+const deleById = async(id: string): Promise<void | Error> => {
+    try {
+        await api.delete<IDetalheClient>(`/api/client${id}`)
+      } catch (error) {
+          console.error(error)
+          return new Error((error as {message: string}).message || 'Erro ao deletar o registro')
+      }
 }
 
 export const ClientService = {
