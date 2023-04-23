880c633004889e65c375847706daa74804ef3d7a
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-04-23 19:47:46 -0300

    caixa divs organizadas

--- src/pages/caixa/CaixaPage.tsx
+++ src/pages/caixa/CaixaPage.tsx
@@ -4,9 +4,25 @@ import {Box} from "@mui/material";
 export const CaixaPage = () => {
 
   return(
-    <Box sx={{background:"#666", width:"100%", padding:"3em 0", display:"flex"}}>
-      <Box sx={{background:"#fff"}}>oi</Box>
-      <Box>
+    <Box
+      sx={{
+        width:"100%",
+        padding:"3em 0",
+        display:"grid",
+        gridTemplateColumns:"1fr 1fr",
+        gap:"1em"
+      }}
+    >
+      <Box sx={{display:"grid", gridTemplateRows:"5em 1fr", rowGap:"1em"}}>
+        <Box  sx={{background:"#fff"}}>oi</Box>
+        <Box  sx={{background:"#fff"}}>io</Box>
+      </Box>
+      <Box sx={{
+          display:"grid",
+          gap:"1em",
+          gridTemplateRows:"1fr 100px 1fr 100px"
+        }}
+      >
         <Box sx={{background:"#fff"}}>1</Box>
         <Box sx={{background:"#fff"}}>2</Box>
         <Box sx={{background:"#fff"}}>3</Box>
