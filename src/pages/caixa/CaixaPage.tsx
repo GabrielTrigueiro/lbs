cb1d3c3e3df22b01cd5c5e709281de17267e0db9
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-05-18 13:18:46 -0300

    configurando tailwind

--- src/pages/caixa/CaixaPage.tsx
+++ src/pages/caixa/CaixaPage.tsx
@@ -1,20 +1,16 @@
 import {Autocomplete, Box, Button, TextField, Typography} from "@mui/material";
-import { Caixa, CaixaContainer, Infos } from "./CaixaComponents";
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import {useState} from "react";
 import {CodeImputField} from "./CodeImputField";
 import {CaixaList} from "./CaixaList";
 
 export const CaixaPage = () => {
 
   return(
-    <CaixaContainer>
-      <Caixa>
+    <div>
+      <div>
         <CodeImputField/>
         <CaixaList/>
-      </Caixa>
-      <Infos>
+      </div>
+      <div>
         <Box sx={{background:"#fff"}}>1</Box>
         <Box sx={{background:"#fff"}}>2</Box>
         <Box sx={{background:"#fff"}}>
@@ -23,7 +19,7 @@ export const CaixaPage = () => {
         <Box sx={{background:"#fff"}}>
           Valor final e confirmar
         </Box>
-      </Infos>
-    </CaixaContainer>
+      </div>
+    </div>
   )
 }
