51e9d45b8f63e5467de628db6c652aa4d8cd233e
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-06 19:42:56 -0300

    login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,96 +1,156 @@
-import Typography from "@mui/material/Typography"
-import { useContext, useEffect, useRef, useState } from "react"
-import AccountCircleIcon from "@mui/icons-material/AccountCircle"
-import LockIcon from "@mui/icons-material/Lock"
-import { VisibilityOff, Visibility } from "@mui/icons-material"
-import { VLoginOutlinedInput } from "../../shared/forms"
-import { Form } from "@unform/web"
-import styles from "../../styles/Login/Login.module.scss"
-import * as Yup from "yup";
-import { FormHandles } from "@unform/core"
-import { SnackbarContext, Snack } from "../../shared/contexts/NotificationContext"
-import {useCallback} from 'react';
+import styled from '@emotion/styled';
+import { useState } from 'react';
 import {
-  InputAdornment,
-  IconButton,
   FormControl,
-  InputLabel,
-  OutlinedInput,
-  CircularProgress,
-  Box,
-  Button,
   Grid,
-  TextField,
-} from "@mui/material"
-import { Notification } from "../../shared/components"
-import { ILogin } from "../../shared/models/user"
-import { AuthService } from "../../shared/services/api/auth/AuthService"
-import { useNavigate } from "react-router"
-import { useAuthContext } from "../../shared/contexts/AuthContext"
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Button,
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { Form } from '@unform/web';
 
+import mulher from 'images/login/Mulher.jpg';
+import { useAuthContext } from 'shared/contexts/AuthContext';
+import 'styles/GlobalVariables.module.scss';
+import {
+  amareloDefault,
+  fundoBranco,
+  amareloClaroDefault,
+} from 'styles/variables';
+import { ILogin } from 'shared/models/user';
+import { CustomInput } from 'shared/forms/forms-components/CustomImput';
+
+const GridContainer = styled(Grid)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+`;
+
+const Mulher = styled.img`
+  flex: 2;
+  background-image: url(${mulher});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: right;
+`;
+
+const LoginContainer = styled(Grid)`
+  flex: 1;
+  background-color: ${fundoBranco};
+  text-align: center;
+  display: flex;
+
+  h2 {
+    font-weight: bold;
+    font-size: 2.5pc;
+  }
+`;
+
+const Formulario = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  padding: 0em 4em;
+`;
+
+const FormularioContainer = styled(Form)`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CustomFormControl = styled(FormControl)`
+  width: 100%;
+  margin: 0.5em !important;
+`;
+
+const CustomButton = styled(Button)`
+  margin-top: 1em !important;
+  width: 50% !important;
+  height: 50px !important;
+  border-radius: 10px !important;
+  font-size: $normal !important;
+  box-shadow: none !important;
+  background-color: ${amareloDefault} !important;
+
+  :hover {
+    background-color: ${amareloClaroDefault} !important;
+    box-shadow: none;
+  }
+`;
+//----------------------------------------------
 export const Login: React.FC = () => {
+  const { login } = useAuthContext();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const {login} = useAuthContext();
-  const [loading, setLoading] = useState(false)
-  const formRef = useRef<FormHandles>(null)
   const [values, setValues] = useState({
-    password: "",
-    username: "",
+    password: '',
+    username: '',
     showPassword: false,
-  })
+  });
 
   const handleClickShowPassword = () => {
     setValues({
       ...values,
       showPassword: !values.showPassword,
-    })
-  }
+    });
+  };
 
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-  }
-  
-  const handleChange = (prop: keyof ILogin) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [prop]: event.target.value })
-  }
-  
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleChange =
+    (prop: keyof ILogin) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
   return (
-    <Grid className={styles.container}>
-      <div className={styles.imagem}/>
-      <Grid className={styles.container_formulario}>
-        <div className={styles.logo}/>
-        <Box className={styles.formulario}>
-          <Typography className={styles.titulo}>Login</Typography>
-          <Form ref={formRef} onSubmit={async (dados) => login(dados)} className={styles.login}>
-            <FormControl className={styles.login_input}  id="outlined-start-adornment">
-              <InputLabel htmlFor="outlined-adornment-user">
-                Usuario
-              </InputLabel>
-              <VLoginOutlinedInput
+    <GridContainer>
+      <Mulher />
+      <LoginContainer>
+        <Formulario>
+          <h2>Login</h2>
+          <FormularioContainer onSubmit={async (dados) => login(dados)}>
+            <CustomFormControl id="outlined-start-adornment">
+              <InputLabel htmlFor="outlined-adornment-user">Usuario</InputLabel>
+              <CustomInput
                 name="username"
                 autoComplete="off"
-                type={"text"}
+                type={'text'}
                 label="Usuário"
                 value={values.username}
-                onChange={handleChange("username")}
-                startAdornment = {
+                onChange={handleChange('username')}
+                startAdornment={
                   <InputAdornment position="start">
                     <AccountCircleIcon />
                   </InputAdornment>
                 }
               />
-              </FormControl>
-            <FormControl className={styles.login_input} variant="outlined">
+            </CustomFormControl>
+            <CustomFormControl variant="outlined">
               <InputLabel htmlFor="outlined-adornment-password">
                 Senha
               </InputLabel>
-              <VLoginOutlinedInput
+              <CustomInput
                 name="password"
                 autoComplete="off"
                 id="outlined-adornment-password"
-                type={values.showPassword ? "text" : "password"}
+                type={values.showPassword ? 'text' : 'password'}
                 value={values.password}
-                onChange={handleChange("password")}
+                onChange={handleChange('password')}
                 startAdornment={
                   <InputAdornment position="start">
                     <LockIcon />
@@ -104,25 +164,30 @@ export const Login: React.FC = () => {
                       onMouseDown={handleMouseDownPassword}
                       edge="end"
                     >
-                      {
-                        values.showPassword ? 
-                        <VisibilityOff /> : <Visibility />
-                      }
+                      {values.showPassword ? (
+                        <VisibilityOffIcon />
+                      ) : (
+                        <VisibilityIcon />
+                      )}
                     </IconButton>
                   </InputAdornment>
                 }
                 label="Senha"
               />
-            </FormControl>
-            <Button className={styles.botao} type="submit" disabled={loading} variant="contained">
+            </CustomFormControl>
+            <CustomButton
+              type="submit"
+              disabled={isLoading}
+              variant="contained"
+            >
               Login
-              {loading && (
-              <CircularProgress className={styles.well}size={24}/>
-            )}
-            </Button>
-          </Form>
-        </Box>
-      </Grid>
-    </Grid>
-  )
-}
+              {/* {loading && (
+                <CircularProgress className={styles.well} size={24} />
+              )} */}
+            </CustomButton>
+          </FormularioContainer>
+        </Formulario>
+      </LoginContainer>
+    </GridContainer>
+  );
+};
---
+++ src/styles/variables.js
@@ -0,0 +1,7 @@
+export const amareloDefault = '#E4DB00';
+export const amareloClaroDefault = '#fffb91';
+export const fundoBranco = '#fff';
+export const fundoCinza = '#575A61';
+export const cinzaEscuro = '#494b4f';
+export const cinzaClaro = '#D9D9D9';
+export const brancoEscuro = '#EFEFEF';
