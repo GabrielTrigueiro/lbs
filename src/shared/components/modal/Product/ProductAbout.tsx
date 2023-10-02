c61fc7d3dab5a88d6a88c5648d6ddedb55d54cbb
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-10-02 17:21:02 -0300

    layout cadastro de produto pronto para receber implementação

--- src/shared/components/modal/Product/ProductAbout.tsx
+++ src/shared/components/modal/Product/ProductAbout.tsx
@@ -25,44 +25,53 @@ export const ProductInfos = ({informacoes}: IProductAbout) => {
           <TextField fullWidth size="small" label={'Nome'} autoComplete="off"/>
           <TextField fullWidth size="small" label={'Descrição'} autoComplete="off"/>
           <TextField fullWidth
-            size="small"
-            label={'Quantidade em estoque'}
-            autoComplete="off"
+                     size="small"
+                     label={'Quantidade em estoque'}
+                     autoComplete="off"
           />
           <TextField fullWidth
-            size="small"
-            label={'Categoria'}
-            autoComplete="off"
+                     size="small"
+                     label={'Categoria'}
+                     autoComplete="off"
           />
         </Box>
       </Box>
       <Box sx={{height: 250, display: 'flex', gap: 1}}>
-        <DataGrid
-          localeText={{ noRowsLabel: "Nenhuma informação cadastrada" }}
-          rows={informacoes.map((info, index) => ({ ...info, id: index + 1 }))}
-          columns={[
-            { field: 'color', headerName: 'Cor', flex: 1 },
-            { field: 'quantity', headerName: 'Quantidade', flex: 1 },
-            { field: 'size', headerName: 'Tamanho', flex: 1 },
-          ]}
-        />
-        <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+        <Box sx={{display: 'flex', flexDirection: "column", gap: 1}}>
+          <DataGrid
+            disableColumnFilter={true}
+            disableColumnMenu={true}
+            disableColumnSelector={true}
+            hideFooter={true}
+            sx={{overflow: "hidden", width: 350}}
+            localeText={{noRowsLabel: "Nenhuma informação cadastrada"}}
+            rows={informacoes.map((info, index) => ({...info, id: index + 1}))}
+            columns={[
+              {field: "color", headerName: "Cor", flex: 1},
+              {field: "quantity", headerName: "Quantidade", flex: 1},
+              {field: "size", headerName: "Tamanho", flex: 1},
+            ]}
+          />
           <Button variant={'contained'}>Adicionar informação</Button>
-          <TextField
-            size="small"
-            label={'Preço custo'}
-            autoComplete="off"
-          />
-          <TextField
-            size="small"
-            label={'Preço etiqueta'}
-            autoComplete="off"
-          />
-          <TextField
-            size="small"
-            label={'Preço venda'}
-            autoComplete="off"
-          />
+        </Box>
+        <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+          <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+            <TextField
+              size="small"
+              label={'Preço custo'}
+              autoComplete="off"
+            />
+            <TextField
+              size="small"
+              label={'Preço etiqueta'}
+              autoComplete="off"
+            />
+            <TextField
+              size="small"
+              label={'Preço venda'}
+              autoComplete="off"
+            />
+          </Box>
           <Button variant={'contained'}>Cadastrar</Button>
         </Box>
       </Box>
